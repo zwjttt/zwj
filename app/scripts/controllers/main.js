e2112fa431dfc90c702152ec73cf4e18aff322e6
zwjttt <zwjttt@163.com>
2016-08-31 15:15:55 +0800

    导入AngularJS测试项目

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name test2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the test2App
+ */
+angular.module('test2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
